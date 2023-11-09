7abbcda643b1fa09584c400f02df6b32cd1c2dfb
Candice819 <40899778+Candice819@users.noreply.github.com>
2023-11-09 15:22:31 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import logo from './img/candice_mckenna.jpg';
+
+function App() {
+  return (
+  <div>
+    <h1>Candice McKenna</h1>
+    <p>Hello! text here ...bla bla lorem ipsum shortcut not working. lame. </p>
+  
+  <figure>
+    <img class="portfolio-img" src={logo} alt="Candice McKenna"/>
+  </figure>
+    </div>
+
+  );
+}
+
+export default App;
